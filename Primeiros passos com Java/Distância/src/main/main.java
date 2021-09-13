6b718c583b748c669bb2f96ff3b8f2083252334a
PauloLucena007 <paulo.henriquegvl@gmail.com>
2021-09-12 21:19:25 -0300

    Repositorio criado e primeiro exercicio solucionado

---
+++ Primeiros passos com Java/Distância/src/main/main.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+        int minutos;
+        Scanner input = new Scanner(System.in);
+        minutos = input.nextInt();
+
+        System.out.printf("%d minutos\n", 2 * minutos  ); // Digite aqui o calculo dos minutos
+    }
+}
+
